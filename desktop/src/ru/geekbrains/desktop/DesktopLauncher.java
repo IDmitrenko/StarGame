00f6d3aa97c0a1accc5d2168276076a9e2de9773
Dmitrenko <Dias64@mail.ru>
2019-06-13 17:00:35 +0300

    homework1

--- desktop/src/ru/geekbrains/desktop/DesktopLauncher.java
+++ desktop/src/ru/geekbrains/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import ru.geekbrains.StarGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "StarGame";
+		config.width = 512;
+		config.height = 1024;
 		new LwjglApplication(new StarGame(), config);
 	}
 }
