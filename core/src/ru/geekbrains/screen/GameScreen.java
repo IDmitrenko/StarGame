242f00abaccc9aa3b5e839888895c75c352affce
Dias <Dias64@mail.ru>
2019-06-22 13:08:55 +0300

    homework4 Оформил GameScreen

--- core/src/ru/geekbrains/screen/GameScreen.java
+++ core/src/ru/geekbrains/screen/GameScreen.java
@@ -1,29 +1,78 @@
 package ru.geekbrains.screen;
 
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 import ru.geekbrains.base.BaseScreen;
 import ru.geekbrains.math.Rect;
+import ru.geekbrains.sprite.Background;
+import ru.geekbrains.sprite.Star;
 
 public class GameScreen extends BaseScreen {
+
+    private static final int STAR_COUNT = 64;
+
+    private Game game;
+
+    private Texture bg;
+    private Background background;
+    private TextureAtlas atlas;
+
+    private Star[] stars = new Star[STAR_COUNT];
+
+    public GameScreen(Game game) {
+        this.game = game;
+    }
 
     @Override
     public void show() {
         super.show();
+        bg = new Texture("textures/bg.png");
+        background = new Background(new TextureRegion(bg));
+        atlas = new TextureAtlas("textures/menuAtlas.tpack");
+        for (int i = 0; i < stars.length; i++) {
+            stars[i] = new Star(atlas);
+        }
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
+        update(delta);
+        draw();
+    }
+
+    public void update(float delta) {
+        for (int i = 0; i < stars.length; i++) {
+            stars[i].update(delta);
+        }
+    }
+
+    public void draw() {
+        batch.begin();
+        background.draw(batch);
+        for (int i = 0; i < stars.length; i++) {
+            stars[i].draw(batch);
+        }
+        batch.end();
     }
 
     @Override
     public void resize(Rect worldBounds) {
         super.resize(worldBounds);
+        background.resize(worldBounds);
+        for (int i = 0; i < stars.length; i++) {
+            stars[i].resize(worldBounds);
+        }
     }
 
     @Override
     public void dispose() {
+        bg.dispose();
+        atlas.dispose();
         super.dispose();
     }
 
